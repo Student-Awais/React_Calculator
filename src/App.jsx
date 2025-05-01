3c4ea7818cc89d05556a4eb0e279049849461c55
Muhammad Awais Abid Hussain <kingyt6624@gmail.com>
2025-05-01 07:58:36 +0500

    Build complete calculator with basic operations

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
+import styles from "./App.module.css";
+import Display from "./components/Display";
+import Buttons from "./components/Buttons";
+import { useState } from "react";
+import React from "react";
 const App = () => {
+  let [displayValue, setDisplayValue] = useState("");
+  const onButtonCLick = (btn) => {
+    if (btn === "C") {
+      setDisplayValue("");
+    } else if (btn === "=") {
+      // const result = eval(displayValue);
+      // setDisplayValue(result);
+      try {
+        const result = eval(displayValue);
+        setDisplayValue(result);
+      } catch {
+        setDisplayValue("Math Error");
+        displayValue = "";
+        setTimeout(() => {
+          setDisplayValue("");
+        }, 2000);
+      }
+    } else {
+      const newValue = displayValue + btn;
+      setDisplayValue(newValue);
+    }
+  };
   return (
-    <div>
-      <h1>Calculator</h1>
-      <p>Welcome to the calculator app!</p>
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <Display displayValue={displayValue}></Display>
+        <Buttons onButtonCLick={onButtonCLick}></Buttons>
+      </div>
     </div>
   );
 };
+
 export default App;
